057d7bd47b6c4ef3ddc28917468d8ab49f7d243b
vemund <vemund.bakken@onio.com>
2023-02-20 13:31:50 +0100

    Small debug

--- board/nrf52840_dk/init.c
+++ board/nrf52840_dk/init.c
@@ -14,6 +14,10 @@ void board_init(void)
 
 void board_uart_init(void)
 {
-    NRF_P0->OUTSET = (1 << 13);
-    NRF_P0->DIRSET = (1 << 13);
+    NRF_P0->OUTSET = (1 << 0x6);
+    NRF_P0->OUTCLR = (1 << 0x8);
+    NRF_P0->DIRSET = (1 << 0x6);
+
+    NRF_UART0->PSEL.TXD = 0x6;
+    NRF_UART0->PSEL.RXD = 0x8;
 }
