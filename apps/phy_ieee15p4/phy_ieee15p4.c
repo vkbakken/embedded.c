1df3989fca39a4e1387cb9266f2b2b541f67deca
Ron Ulysses Swanson <vemund.bakken@onio.com>
2023-02-20 13:30:04 +0100

    Work on posix code

---
+++ apps/phy_ieee15p4/phy_ieee15p4.c
@@ -0,0 +1,26 @@
+#include <stdint.h>
+#include <stdbool.h>
+
+
+#include <console.h>
+#include <init.h>
+
+
+int main(void)
+{
+    board_init();
+    console_init();
+
+    dbg_panic("Console panic\r\n");
+    dbg_error("Console error\r\n");
+    dbg_warning("Console warning\r\n");
+    dbg_info("Console info\r\n");
+    dbg_verbose("Console verbose\r\n");
+
+    
+    while (true) {
+        console_realine();
+    }
+
+    return 0;
+}
---
+++ utils/stream/debug.c
@@ -0,0 +1,61 @@
+#include <assert.h>
+#include <debug.h>
+#include <stream.h>
+#include <uart.h>
+#include <uart/ctrl.h>
+#include <util.h>
+
+
+#ifdef CONFIG_DEBUG_BUF_SIZE
+# define DEBUG_BUF_SIZE	CONFIG_DEBUG_CONSOLE_BUF_SIZE
+#else
+# define DEBUG_BUF_SIZE	64
+#endif
+
+
+static char	out_buffer[DEBUG_BUF_SIZE];
+
+
+struct stream	out_stream = {
+	.ring_mask	= DEBUG_BUF_SIZE - 1,
+	.data		= out_buffer,
+};
+
+
+int dbg_vprintf(const char *format, va_list ap)
+{
+	return stream_vprintf(&out_stream, format, ap);
+}
+
+
+int dbg_printf(const char *format, ...)
+{
+	va_list ap;
+	int n;
+
+	va_start(ap, format);
+	n = stream_vprintf(&out_stream, format, ap);
+	va_end(ap);
+
+	return n;
+}
+
+
+int dbg_putstr(const char *str)
+{
+	return stream_putstr(&out_stream, str);
+}
+
+
+int dbg_putchar(int c)
+{
+	return stream_putchar(&out_stream, c);
+}
+
+
+
+void dbg_init(void)
+{
+	build_assert(is_power_of_two(DEBUG_BUF_SIZE));
+	dbg_backend_init(&out_stream);
+}
